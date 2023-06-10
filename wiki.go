136a52f0fd40e524f72d1b9ccc8b941c56ac2201
hackeramitkumar <amit9116260192@gmail.com>
2023-06-10 10:20:32 +0530

    added viewhandler

--- wiki.go
+++ wiki.go
@@ -26,18 +26,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main3() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample k Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div></div>", p.Title)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// p1 := new Page( "amit",[]byte("My name is Amit!!!"));
+	// p1.save();
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8088", nil))
 }
